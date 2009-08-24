d1e98c925e84872cff6ce239d567227945d5707a
Jaime Gutierrez <jamescr@gmail.com>
2009-08-24 15:52:07 +0000

    el SpecimenDTO tenía problemas creando DTO's que fueran instancias de clases hijas de Specimen (por ejemplo INBioSpecimen, ARASpecimen, etc, etc) ya se arreglo esto.

--- m3s-service/src/main/java/org/inbio/m3s/dto/taxonomy/SpecimenLiteDTOFactory.java
+++ m3s-service/src/main/java/org/inbio/m3s/dto/taxonomy/SpecimenLiteDTOFactory.java
@@ -3,6 +3,7 @@
  */
 package org.inbio.m3s.dto.taxonomy;
 
+import org.apache.log4j.Logger;
 import org.inbio.m3s.dto.BaseDTOFactory;
 import org.inbio.m3s.model.core.SpecimenMediaId;
 import org.inbio.m3s.model.general.Specimen;
@@ -12,18 +13,29 @@ import org.inbio.m3s.model.general.Specimen;
  *
  */
 public class SpecimenLiteDTOFactory extends BaseDTOFactory {
+	
+	private static Logger logger = Logger.getLogger(SpecimenLiteDTOFactory.class);
 
 	/* (non-Javadoc)
 	 * @see org.inbio.m3s.dto.DTOFactory#createDTO(java.lang.Object)
 	 */
 	public Object createDTO(Object entity) {
-		if(entity == null)
-				return null;
 		
-		else if(entity.getClass() == Specimen.class){
+		if(entity == null){
+			logger.debug("createDTO with a null entity");
+			return null;
+				
+		}
+		
+		logger.debug("createDTO with a: "+entity.getClass().toString());
+		
+		if(entity instanceof Specimen){
+			logger.debug("createDTO with a Specimen class");
 			Specimen s = (Specimen) entity;
 			return new SpecimenLiteDTO(s.getSpecimenId().toString());
-		} else if(entity.getClass() == SpecimenMediaId.class){
+		
+		} else if(entity instanceof SpecimenMediaId){
+			logger.debug("createDTO with a SpecimenMediaId class");
 			SpecimenMediaId smId = (SpecimenMediaId) entity;
 			return new SpecimenLiteDTO(String.valueOf(smId.getSpecimenId()));
 		}
