25808f497969106ac43d590cef12e6f8e08da7de
Esteban Mata <esteban81mata@gmail.com>
2010-05-28 16:57:51 +0000

    ...

--- m3s-service/setup.sh
+++ m3s-service/setup.sh
@@ -45,17 +45,17 @@ sudo apt-get install imagemagick
 echo ""
 echo "Creating database [password for superuser will be prompt]"
 #Uncomment this line to create a role for the application
-psql --host localhost --port 5432 --username postgres --password < m3s_role[2010-05-27].sql
+psql --host localhost --port 5432 --username postgres --password < db/m3s_role[2010-05-27].sql
 
 #data base schema creation
-psql --host localhost --port 5432 --username postgres --password < m3s_schema[2010-05-27].sql
+psql --host localhost --port 5432 --username postgres --password < db/m3s_schema[2010-05-27].sql
 
 #populate the database using inbio data
 #pg_dump --host 10.0.0.102 --port 5432 --username m3s --password --format plain --data-only --table core.text --table core.text_translation --table core.media_attribute --table core.media_attribute_type --table core.metadata_standard --table core.media_category --table core.media_type --table core.media_use --table core.privilege --table core.project --table core.security_users --table core.system_user --table core.user --table core.user_project_privilege --table core.use_policy  --table core.keyword_category --table core.keyword --file populate_inbio_data[2010-05-27].sql
-psql --host localhost --port 5432 --username postgres --password --dbname m3s  < populate_inbio_data[2010-05-27].sql
+psql --host localhost --port 5432 --username postgres --password --dbname m3s  < db/populate_inbio_data[2010-05-27].sql
 
 #geo tagging update
-psql --host localhost --port 5432 --username postgres --password --dbname m3s  < update_geotagging.sql
+psql --host localhost --port 5432 --username postgres --password --dbname m3s  < db/update_geotagging.sql
 
 
 echo ""
